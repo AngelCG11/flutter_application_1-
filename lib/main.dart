e0f8fc1da4b327ab5c328c7cce9fc636e18291a2
AngelCG11 <cardenas.gtz.angel@gmail.com>
2023-11-06 23:00:57 -0600

    LDSW 3.4. Utilización de widgets
    En esta actividad configure los widgets: Text, Row, Column, Stack y Container en la aplicación móvil que estoy desarrollando.

--- lib/main.dart
+++ lib/main.dart
@@ -35,14 +35,43 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Hello World'),
-          Text(appState.current.asLowerCase),
-        ],
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text('Utilización de widgets Angel Cardenas Gutierrez', style: TextStyle(fontSize: 24)),
+            Text(appState.current.asLowerCase),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text('Elemento 1'),
+                Text('Elemento 2'),
+                Text('Elemento 3'),
+              ],
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: 300,
+                  height: 300,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
 
